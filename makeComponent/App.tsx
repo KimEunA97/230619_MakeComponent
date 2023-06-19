3a42922f762144088faf389758cbdede50762091
KimEunA97 <kimeuna9712@gmail.com>
2023-06-19 13:48:56 +0900

    소방차 타이머 작업완료

--- makeComponent/App.tsx
+++ makeComponent/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SOSToggleBox from './src/component/ToggloeBtn';
+import SobangchaBox from './src/component/sobangchaBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <SOSToggleBox></SOSToggleBox>
+      <SobangchaBox></SobangchaBox>
     </View>
   );
 }
